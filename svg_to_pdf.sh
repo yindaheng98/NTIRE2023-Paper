c0e3a7596eebb557773728b18b5fe14eadb89e16
yindaheng98 <yindaheng98@gmail.com>
2023-03-19 20:25:36 +0800

    图都ok

---
+++ svg_to_pdf.sh
@@ -0,0 +1,4 @@
+inkscape --export-filename=RFDN.pdf RFDN.svg
+inkscape --export-filename=Branching.pdf Branching.svg
+inkscape --export-filename=Re-parametrization.pdf Re-parametrization.svg
+inkscape --export-filename=Pruning.pdf Pruning.svg
