ef8065de89f19c756f89ed7d37cc279e9f1a5e60
Liu Mengyang <myliu@seu.edu.cn>
2023-04-05 23:31:17 +0800

    Update introduction and method of ESRx4 paper

--- svg_to_pdf.sh
+++ svg_to_pdf.sh
@@ -2,3 +2,6 @@ inkscape --export-filename=RFDN.pdf RFDN.svg
 inkscape --export-filename=Branching.pdf Branching.svg
 inkscape --export-filename=Re-parametrization.pdf Re-parametrization.svg
 inkscape --export-filename=Pruning.pdf Pruning.svg
+inkscape --export-filename=expand.pdf expand.svg
+inkscape --export-filename=shrink.pdf shrink.svg
+inkscape --export-filename=expandandshrink.pdf expandandshrink.svg
